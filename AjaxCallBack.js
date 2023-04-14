10e5628713eea8f7c79bebf2908e5bc84111df59
Subhash Barela <barelasubhash16@gmail.com>
2023-04-14 13:58:21 +0530

    scync emp data using ajax and json

--- AjaxCallBack.js
+++ AjaxCallBack.js
@@ -26,11 +26,37 @@ function getUserDetails(data) {
 }
 makeAJAXCall("GET", getURL, getUserDetails);
 console.log(" Made GET AJAX Call to server at: " + showTime());
-const postURL = "http://localhost:3000/employees";
 
-const empData = { "name": "Raaj", "salary": "345000" };
+const deleteURL="http://localhost:3000/employees/4";
+function userDeleted(data){
+console.log(" User Deleted  "+data)
+}
+makeAJAXCall ("DELETE", deleteURL, userDeleted, false);
+
+const postURL = "http://localhost:3000/employees";
+const empData = { "name": "Harry", "salary": "5000" };
 function userAdded(data) {
     console.log(" User Added: " + data)
     console.log("User Added at: " + showTime() + "data: " + data)
 }
 makeAJAXCall("POST", postURL, userAdded, true, empData);
+
+
+function makeAJAXCall(methodType, url, callback, async=true, data=null){
+    let xhr=new XMLHttpRequest ();
+    xhr.onreadystatechange = function(){    
+       console.log("State changed called. Ready state: "+ xhr.readyState+ " Status:"+xhr.status);
+       if(xhr.readyState === 4)
+       {
+        //Matching all 200 series Reponses
+        if(xhr.status ===200 || xhr.status === 201)
+        {
+            callback(xhr.responseText);
+        }
+        else if(xhr.status >= 400)
+        {
+            console.log("Handle 400 client error or 500 server Error");
+        }
+    }
+    }
+}
